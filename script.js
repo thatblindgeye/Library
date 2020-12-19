ad4079c1b1eb471e2d72ee9e6e077e3456bdb6c8
thatblindgeye <thatblindgeye@gmail.com>
2020-12-19 11:21:39 -0500

    Add CSS responsiveness
    Added responsiveness with CSS to work for mobile-sized and desktop-sized
    screens. Also added test scripts for possible features.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 "use strict";
 const addBtn = document.querySelector(".add-btn");
+const editBtn = document.querySelector(".edit-book");
+const deleteBtn = document.querySelector(".delete-book");
 const resetBtn = document.querySelector(".reset-btn");
+const scrollBtn = document.querySelector(".scroll-btn");
+const statusBtn = document.querySelector(".book-status");
+
 const searchBox = document.getElementById("search");
 const searchDropdown = document.getElementById("search-type");
 const sortDropdown = document.getElementById("sort-dropdown");
@@ -18,15 +23,16 @@ function toggleTheme() {
 }
 
 function toggleOptions() {
-  if (visibility === false) {
-    document.querySelector(".options-container").style.display = "flex";
-    optionsToggle.innerHTML = "<b>HIDE LIBRARY OPTIONS<b>";
-    visibility = true;
-  } else if (visibility === true) {
-    document.querySelector(".options-container").style.display = "none";
-    optionsToggle.innerHTML = "<b>SHOW LIBRARY OPTIONS<b>";
-    visibility = false;
-  }
+    if (visibility === false) {
+      document.querySelector(".options-container").style.display = "flex";
+      optionsToggle.innerHTML = '<b>HIDE LIBRARY OPTIONS<i class="material-icons">arrow_drop_up</i><b>';
+      visibility = true;
+    } else if (visibility === true) {
+      document.querySelector(".options-container").style.display = "none";
+      optionsToggle.innerHTML = '<b>SHOW LIBRARY OPTIONS<i class="material-icons">arrow_drop_down</i><b>';
+      visibility = false;
+    }
+
 }
 
 function resetOptions() {
@@ -36,9 +42,67 @@ function resetOptions() {
   sortDropdown.value = "added newest";
 }
 
-window.addEventListener("load", toggleTheme)
+function toggleScrollBtn() {
+  const libraryContainer = document.querySelector(".library-container");
+  if (document.body.scrollWidth < 710 && window.pageYOffset > libraryContainer.offsetTop || 
+    document.body.scrollWidth > 710 && window.pageYOffset > 765) {
+    document.querySelector(".scroll-btn").style.display = "block";
+  } else {
+    document.querySelector(".scroll-btn").style.display = "none";
+  }
+}
+
+window.addEventListener("scroll", toggleScrollBtn)
+
+window.addEventListener("resize", () => {
+  if (document.body.scrollWidth < 710 && visibility === false) {
+    document.querySelector(".options-container").style.display = "none";
+  } else if (document.documentElement.scrollWidth >= 710) {
+    document.querySelector(".options-container").style.display = "flex";
+  }
+})
+
+window.addEventListener("load", () => {
+  toggleTheme();
+  toggleScrollBtn();
+  if (document.documentElement.scrollWidth >= 710) {
+    document.querySelector(".options-container").style.display = "flex";
+  }
+})
+
 themeSelect.addEventListener("change", toggleTheme)
 
 optionsToggle.addEventListener("click", toggleOptions)
 
 resetBtn.addEventListener("click", resetOptions)
+
+scrollBtn.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
+})
+
+document.querySelector(".book").addEventListener("click", () => {
+  if (document.querySelector(".book").style.backgroundImage === "none") {
+    document.querySelector(".book").style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/en/e/e0/Game_of_Thrones_Season_8.png')";
+    document.querySelector(".book-title").style.display = "none";
+    document.querySelector(".book-author").style.display = "none";
+    document.querySelector(".book-bottom").style.display = "none";
+  } else {
+    document.querySelector(".book").style.backgroundImage = "none";
+    document.querySelector(".book-title").style.display = "block";
+    document.querySelector(".book-author").style.display = "block";
+    document.querySelector(".book-bottom").style.display = "flex";
+  }
+
+})
+
+statusBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+})
+
+editBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+})
+
+deleteBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+})
